97303129583fd6c2d054a426af5ac73e7975c6da
larsroettig <larsroettig+github@gmail.com>
2023-08-30 09:48:14 +0200

    3 files modified

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 import express from 'express';
+import proxy from 'express-http-proxy';
 const app = express();
 import * as http from 'http';
 const server = http.createServer(app);
 import { Server } from 'socket.io';
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Chat Server</h1>');
-});
+app.use('/', proxy('http://localhost:5173/'));
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(8080, () => {
+  console.log('listening on *:8080');
 });
