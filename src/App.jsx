c6a475bd744ab2447d11204743f262a61daac8c4
larsroettig <larsroettig+github@gmail.com>
2023-08-30 10:07:25 +0200

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import Navbar from './components/navbar';
 import LiveChat from './routes/chat';
 import HomePage from './routes/home';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/livechat" component={LiveChat} />
-        </Switch>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="livechat" element={<LiveChat />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
